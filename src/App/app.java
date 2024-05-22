2447b513c57a01034110b18b3657a6bb93d50f6b
antoninlancesseur@gmail.com <antoninlancesseur@gmail.com>
2024-05-22 18:51:31 +0200

    ajout du main

---
+++ src/App/app.java
@@ -0,0 +1,21 @@
+package App;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.*;
+import modeles.*;
+import vues.*;
+import controleurs.*;
+
+
+public class app {
+
+	public static void main(String[] args) {
+		
+		// Creation de la fenetre de l'app
+		JFrame app = new JFrame();
+		
+		
+		
+	}
+
+}
