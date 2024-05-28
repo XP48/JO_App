81b263bbe31ac8659c302caaea5ed63b900f8488
Antonin Lancesseur <antonin.lancesseur@universite-paris-saclay.fr>
2024-05-28 11:30:17 +0200

    ajout des boutons sur l'accueil

--- src/vues/Accueil.java
+++ src/vues/Accueil.java
@@ -1,6 +1,7 @@
 package vues;
 import javax.swing.*;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 
 public class Accueil extends JPanel {
@@ -8,21 +9,37 @@ public class Accueil extends JPanel {
 	JTable Planning;
 	JButton Equipes;
 	JButton Epreuves;
+	JButton Accueil;
 	
 	
 	public JPanel main = new JPanel();
+	
+	JPanel top = new JPanel();
 	
 	
 	public Accueil() {
 		
 		main.setLayout(new BorderLayout());
 		
+		top.setLayout(new BorderLayout());
+		
 		Planning = new JTable(10,5);
+		
+		Equipes = new JButton("Equipes");
+		
+		Accueil = new JButton("Accueil");
+		Accueil.setEnabled(false);
+		
+		Epreuves = new JButton("Epreuves");
 		
 		main.add(Planning, BorderLayout.CENTER);
 		
+		top.add(Equipes, BorderLayout.WEST);
+		top.add(Accueil, BorderLayout.CENTER);
+		top.add(Epreuves, BorderLayout.EAST);
+		
 		// Ajout de JPanel sur les cotes pour creer de la marge
-		main.add(new JPanel(), BorderLayout.NORTH);
+		main.add(top, BorderLayout.NORTH);
 		main.add(new JPanel(), BorderLayout.WEST);
 		
 	}
