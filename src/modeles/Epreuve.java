0ed784a78ce5389f8ec4c33025935a7239d59caa
Alexis Tirant <alexis.tirant@universite-paris-saclay.fr>
2024-06-03 11:39:57 +0200

    debug session epreuve

--- src/modeles/Epreuve.java
+++ src/modeles/Epreuve.java
@@ -2,33 +2,33 @@ package modeles;
 
 import java.util.*;
 
-public abstract class Epreuve {
+public class Epreuve {
 
 	private ArrayList<Medaille> sesMedaille = new ArrayList<Medaille>();
 	Lieu sonLieu;
 	private ArrayList<Equipe> sesParticipants = new ArrayList<Equipe>();
 	private ArrayList<Session> sesSession = new ArrayList<Session>();
+	public ArrayList<Epreuve> lesEpreuves = new ArrayList<Epreuve>();
 	private int idEpreuve;
 	protected String nomEpreuve;
-	
-	
 	private static int nbEpreuve = 0;
 	private Medaille Medaille;
 
+	
 	public  String getnomEpreuve() {
 		return nomEpreuve;
 	}
-	
 	public int getIdEpreuve() {
 		return this.idEpreuve;
 	}
-
 	public String getNomEpreuve() {
 		return this.nomEpreuve;
 	}
 	public Epreuve(String nomEpreuve) {
+		this.idEpreuve=nbEpreuve++;
 		this.nomEpreuve=nomEpreuve;
 	}
+	
 	public void ajouteSessionE(Session s) {
 		sesSession.add(s);
 	}
